f8f0d8885eb3fb5e4b1a9e6b9e5a985e3f9478f5
IvelinStanchev <telerika123@gmail.com>
2014-06-12 17:35:10 +0300

    main funtions

--- AvoidCarsGame/AvoidCarsGame/scripts/AvoidCarsGame.js
+++ AvoidCarsGame/AvoidCarsGame/scripts/AvoidCarsGame.js
@@ -1,46 +1,147 @@
 ﻿window.onload = function () {
     var canvas = document.getElementById("field");
     var ctx = canvas.getContext("2d");
-    var fieldMinPositionX = 350;
-    var fieldMaxPositionX = 850;
-    var fieldPositionY = 580;
+    
+    var pointsDiv = document.getElementById("points");
 
     var myCarImage = new Image();
     myCarImage.src = 'images/car.png';
-
-    var myCar = new MainCar(565, 450, myCarImage);
-
+    var obstacleCarImage = new Image();
+    obstacleCarImage.src = 'images/obstacleCar.png';
+    
     var directions = {
         "left": -175,
         "right": 175,
         "center": 0
     };
 
-    myCar.draw("center");
+    var minCanvasHeight = 0;
+    var maxSpeed = 20;
+    var fieldMinPositionX = 350;
+    var fieldMaxPositionX = 850;
+    var centerPosition = ((fieldMaxPositionX + fieldMinPositionX) / 2) - 35;
+    var fieldPositionY = canvas.height;
+    var mainCarDirection = "center";
+    var playerPoints = 0;
+
+    var myCar = new MainCar(centerPosition, canvas.height - 140, myCarImage, mainCarDirection);
+    var obstacleCar = new ObstacleCar(centerPosition, minCanvasHeight - 128, obstacleCarImage, "center");
+
+    var obstacleCars = [];
+    obstacleCars.push(obstacleCar);
+
+    requestAnimationFrame(drawingObjects);
+    var updateObstacleCarY = 0.5;
+    //var cyclesCount = 0;
+
+    function drawingObjects() {
+        //cyclesCount++;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        drawObstacleCars(updateObstacleCarY);
+
+        myCar.draw(mainCarDirection);
+
+        checkForCollision();
+
+        checkIfAddingANewObstacleCarIsNeeded();
+
+        deleteOuterObstacleCars();
+
+        if (!(updateObstacleCarY >= maxSpeed)) {
+            updateObstacleCarY += 0.005;
+        }
+
+        pointsDiv.innerHTML = playerPoints;
+
+        requestAnimationFrame(drawingObjects);
+    }
+
+    function ObstacleCar(x, y, image, line) {
+        this.x = x;
+        this.y = y;
+        this.image = image;
+        this.line = line;
+
+        this.draw = function (update) {
+            this.y += update;
+            ctx.drawImage(this.image, this.x, this.y);
+            this.backBumperY = this.y + 128;
+            this.frontBumperY = this.y;
+        }
+    }
 
     function MainCar(x, y, image) {
         this.x = x;
         this.y = y;
         this.image = image;
-        this.frontBumperY = 450;
+        this.frontBumperY = this.y;
+        this.backBumperY = this.y + 128;
         this.frontLeftBumperX = 570;
         this.frontRightBumperX = 620;
+
         this.draw = function (direction) {
             var newPosition = this.x + directions[direction];
+
             if (!(newPosition > fieldMaxPositionX) && !(newPosition < fieldMinPositionX)) {
-                this.x = newPosition;
-                ctx.clearRect(0, 0, canvas.width, canvas.height);
                 ctx.drawImage(this.image, newPosition, this.y);
+                this.line = mainCarDirection;
             }
         }
     }
 
-    function drawMyCar(mainCar, direction, mainCarY) {
-        var newPosition = myCarX + directions[direction];
-        if (!(newPosition > fieldMaxPositionX) && !(newPosition < fieldMinPositionX)) {
-            myCarX = newPosition;
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            ctx.drawImage(mainCar, newPosition, mainCarY);
+    function drawObstacleCars(updateObstacleCarY) {
+        for (var i = 0, len = obstacleCars.length; i < len; i++) {
+            obstacleCars[i].draw(updateObstacleCarY);
+        }
+    }
+
+    function deleteOuterObstacleCars() {
+        for (var i = 0; i < obstacleCars.length; i++) {
+            if (obstacleCars[i].y > canvas.height) {
+                playerPoints += 5;
+                obstacleCars.splice(i, 1);
+            }
+        }
+    }
+
+    function checkIfAddingANewObstacleCarIsNeeded() {
+        var isNeeded = true;
+
+        for (var i = 0, len = obstacleCars.length; i < len; i++) {
+            if (obstacleCars[i].y < (canvas.height / 2)){
+                isNeeded = false;
+            }
+        }
+
+        if (isNeeded) {
+            var generatedRandomValue = generateRandomNumberFrom0To2();
+            var direction;
+
+            if (generatedRandomValue === 0) {
+                direction = "left";
+            }
+            else if (generatedRandomValue === 1) {
+                direction = "right";
+            }
+            else {
+                direction = "center";
+            }
+
+            obstacleCars.push(new ObstacleCar(centerPosition + directions[direction],
+                minCanvasHeight - 128,
+                obstacleCarImage,
+                direction));
+        }
+    }
+
+    function checkForCollision() {
+        for (var i = 0, len = obstacleCars.length; i < len; i++) {
+            if ((obstacleCars[i].backBumperY > myCar.frontBumperY) && (obstacleCars[i].frontBumperY + 10 < myCar.backBumperY)) {
+                if (obstacleCars[i].line == myCar.line) {
+                    alert("Game Over! Points: " + playerPoints);
+                }
+            }
         }
     }
 
@@ -49,15 +150,34 @@
         e = e || window.event;
 
         if (e.keyCode == '40') {
-            direction = 'down';
+            //brake
+            if (updateObstacleCarY > 1) {
+                updateObstacleCarY -= 0.5;
+            }
+            
+            if (playerPoints >= 15) {
+                playerPoints -= 15;
+            }
         }
         else if (e.keyCode == '39') {
-            direction = 'right';
-            myCar.draw(direction);
+            if (mainCarDirection == "center") {
+                mainCarDirection = 'right';
+            }
+            if (mainCarDirection == "left") {
+                mainCarDirection = 'center';
+            }
         }
         else if (e.keyCode == '37') {
-            direction = 'left';
-            myCar.draw(direction);
+            if (mainCarDirection == "center") {
+                mainCarDirection = 'left';
+            }
+            if (mainCarDirection == "right") {
+                mainCarDirection = 'center';
+            }
         }
+    }
+
+    function generateRandomNumberFrom0To2() {
+        return (Math.random()) * 3 | 0;
     }
 }
